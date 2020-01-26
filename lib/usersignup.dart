83249444c5eb88f18f87694edbfa079bb78b0cdb
Aswin M Prabhu <aswinmprabhu@gmail.com>
2020-01-26 07:33:55 +0530

    Add login and signup pages

--- lib/usersignup.dart
+++ lib/usersignup.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hackverse/userwarning.dart';
+
+TextEditingController name1 = new TextEditingController();
+TextEditingController name2 = new TextEditingController();
+TextEditingController name3 = new TextEditingController();
+TextEditingController email = new TextEditingController();
+TextEditingController password = new TextEditingController();
 
 class UserSignUp extends StatelessWidget {
   @override
@@ -9,39 +16,58 @@ class UserSignUp extends StatelessWidget {
       ),
       body: Container(
           padding: EdgeInsets.all(20.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                'SIGN UP',
-                style: TextStyle(fontSize: 20),
-              ),
-
-
-              TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(labelText: "First Name")),
-              TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(labelText: "Middle Name")),
-              TextFormField(
-                  keyboardType: TextInputType.text,
-                  decoration: InputDecoration(labelText: "Last Name ")),
-              TextFormField(
-                  keyboardType: TextInputType.phone,
-                  decoration: InputDecoration(labelText: "Phone Number ")),
-              TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(labelText: "Email Address ")),
-              TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(labelText: "Password ")),
-              RaisedButton(child: Text("SIGN UP"), onPressed: () {}),
-
-
-            ],
-          )
-      ),
-
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  'SIGN UP',
+                  style: TextStyle(fontSize: 20),
+                ),
+                TextFormField(
+                    controller: name1,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(labelText: "First Name")),
+                TextFormField(
+                    controller: name2,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(labelText: "Middle Name")),
+                TextFormField(
+                    controller: name3,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(labelText: "Last Name ")),
+                TextFormField(
+//                    controller: phone,
+                    keyboardType: TextInputType.phone,
+                    decoration: InputDecoration(labelText: "Phone Number ")),
+                TextFormField(
+                    controller: email,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: "Email Address ")),
+                TextFormField(
+                    controller: password,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password ")),
+                RaisedButton(
+                  child: Text("SIGN UP"),
+                  onPressed: () {
+                    validateStructure(password.text);
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => UserWarning()));
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: new BorderRadius.circular(80.0)),
+                ),
+              ],
+            ),
+          )),
     );
+  }
+
+  bool validateStructure(String value) {
+    String pattern =
+        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+    RegExp regExp = new RegExp(pattern);
+    return regExp.hasMatch(value);
   }
 }
---
+++ lib/userwarning.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class UserWarning extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('WARNING'),
+        ),
+        body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    'All users must under any  circumstance refrain from sharing your personal information such as name, phone number , address or other contact details to anyone via the app . Always refer to oneself by the username provided.  Sharing of information during or post ‘Meet Ups’ is optional and is advised to be under the supervision of the ‘moderator’. The above said rule is to ensure a good experience and more importantly the safety of the user. Hence users are expected to strictly stick by the said rules .',
+                    style: TextStyle(fontSize: 23.0),
+                  ),
+                  RaisedButton(
+                    child: Text(
+                      'I Agree',
+                      style: TextStyle(fontSize: 25.0),
+                    ),
+                    onPressed: () {}, //Navigate to home
+                    shape: RoundedRectangleBorder(
+                        borderRadius: new BorderRadius.circular(80.0)),
+                  )
+                ])));
+  }
+}
